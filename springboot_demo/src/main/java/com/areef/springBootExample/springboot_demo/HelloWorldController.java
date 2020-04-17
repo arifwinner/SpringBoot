c78c2be94ff929c850fcb0250fb650fc3b146f20
Areefulla <MAreefulla@ad.harman.com>
2020-04-17 08:35:13 +0530

    Added  internationalization functionality

--- springboot_demo/src/main/java/com/areef/springBootExample/springboot_demo/HelloWorldController.java
+++ springboot_demo/src/main/java/com/areef/springBootExample/springboot_demo/HelloWorldController.java
@@ -1,13 +1,22 @@
 package com.areef.springBootExample.springboot_demo;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/hello")
 	public String helloWorld()
@@ -18,7 +27,7 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world")
 	public String helloWorld2()
 	{
-		return "Hello World 2";
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 
 	@GetMapping(path = "/hello-world-bean")
--- springboot_demo/src/main/java/com/areef/springBootExample/springboot_demo/SpringbootDemoApplication.java
+++ springboot_demo/src/main/java/com/areef/springBootExample/springboot_demo/SpringbootDemoApplication.java
@@ -1,7 +1,16 @@
 package com.areef.springBootExample.springboot_demo;
 
+import java.util.Locale;
+
+import javax.websocket.Session;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class SpringbootDemoApplication {
@@ -10,4 +19,22 @@ public class SpringbootDemoApplication {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver locareResolver() {
+		//SessionLocaleResolver localeResolver = new  SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	/*
+	 * Instead of this configuration we can add one property sppring.messages.basename to application.property file
+	 * @Bean public ResourceBundleMessageSource bundleMessageSource() {
+	 * 
+	 * ResourceBundleMessageSource messageSource = new
+	 * ResourceBundleMessageSource(); messageSource.setBasename("messages"); return
+	 * messageSource;
+	 * 
+	 * }
+	 */
 }
